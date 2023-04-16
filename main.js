ad0c7ff8de8b5bbc1ab1285e090e2191017df56c
jarolthecoder <jarolr.webdev@gmail.com>
2023-04-16 14:35:35 -0500

    Updated

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ window.addEventListener('scroll', ()=> {
 	if(window.scrollY > 0) {
 		navbar.classList.add('active-scroll');
 		navbar.classList.add('shadow');
+
 	} else {
 		navbar.classList.remove('active-scroll');
 		navbar.classList.remove('shadow');
